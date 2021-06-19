d71099c567a2132cc6e6a280131c1f61f55c528c
XDEV200 <xdev200200@gmail.com>
2021-06-19 11:20:05 +0530

    Revert "update: custom html tag to div"
    This reverts commit 66285d701cc5772cd18b817d92fe853c9393c76f.

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,48 +1,44 @@
-
 import "./login.module.css";
 export const Login = () => {
-
- 
-  
   return (
-    <div className="h-screen flex items-center justify-center w-full overflow-hidden ">
-      <div className="h-auto lg:w-1/4 w-3/4 bg-white  rounded-lg relative text-center  lg:border lg:border-gray-300">
-        <div>
+    <container className="h-screen flex items-center justify-center w-full overflow-hidden ">
+      <login className="h-auto lg:w-1/4 w-3/4 bg-white  rounded-lg relative text-center  lg:border lg:border-gray-300">
+        <heading>
           <h1 className=" lg:text-5xl text-3xl p-4 w-full  font-bold">NOTE-KEEP</h1>
-          <p className="text-2xl">Sign in</p>
-        </div>
-        <div className='flex flex-col  mt-12'>
-          <div className="flex justify-center w-full">
+          <span className="text-2xl">Sign in</span>
+        </heading>
+        <inputs className='flex flex-col  mt-12'>
+          <box className="flex justify-center w-full">
             <label>
               <input
                 type="email"
                 className="border py-4 lg:w-96 focus:outline-none focus:border-blue-500"
               />
-              <span className="ml-1 ">Email</span>
+              <placeholder className="ml-1 ">Email</placeholder>
             </label>
             
-          </div>
-          <div className="flex justify-center mt-12 w-full">
+          </box>
+          <box className="flex justify-center mt-12 w-full">
             <label>
               <input
                 type="password"
                 className="border py-4 lg:w-96 focus:outline-none focus:border-blue-500"
               />
-              <span className="ml-1 ">Password</span>
+              <placeholder className="ml-1 ">Password</placeholder>
             </label>
-          </div>
-          <div className="flex justify-center mt-4 w-full">
+          </box>
+          <box className="flex justify-center mt-4 w-full">
                 <a href='!#' className='text-blue-500 text-1xl'>Forget Password?</a>
 
-          </div>
-          <div className="flex justify-center mt-16 mb-12 w-full">
+          </box>
+          <box className="flex justify-center mt-16 mb-12 w-full">
               <button className=' text-2xl p-4 bg-blue-500 lg:w-96 w-44 text-white'>LOGIN</button>
-          </div>
-          <div className="flex justify-center  mb-8 w-full">
+          </box>
+          <box className="flex justify-center  mb-8 w-full">
               <p className='text-1xl'>Not registered? Create an account</p>
-          </div>
-        </div>
-      </div>
-    </div>
+          </box>
+        </inputs>
+      </login>
+    </container>
   );
 };
